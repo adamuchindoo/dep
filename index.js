de1f5a0775a0ebe64033758ed1ffa1ee732c1cd4
adamuchindoo <49189545+adamuchindoo@users.noreply.github.com>
2022-03-24 08:54:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const product=require("./api/product");
 app.use(express.json({extended:false}));
 app.use("/api/product",product);
 
-const port=process.env.port || 8080;
+const port=8080;
 app.listen(port,()=>console.log('service is r ${PORT}'));
